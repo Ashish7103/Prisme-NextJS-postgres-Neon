aff2bdc95d3e174220424dedbc0707d29f294714
Ashish7103 <aksingh7103ashu@gmail.com>
2025-08-06 14:09:50 +0530

    "prisma added dependency to next app"

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import {client} from "@repo/prisma/client";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
